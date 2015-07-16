944e6392c0e7a80b2197f32a2646bd14250b13cb
Pintu Lal <pintulalmee@gmail.com>
2015-07-16 22:51:05 +0530

    cloud link for js css

--- header.php
+++ header.php
@@ -9,22 +9,23 @@
   <meta name="devloper" content="Pintu Lal meena">
   <meta name="copyright" content="jefto.com">
 
-  <html>
+  
  
       <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="./materialize/css/materialize.min.css"  media="screen,projection"/>
+      <!-- <link type="text/css" rel="stylesheet" href="./materialize/css/materialize.min.css"  media="screen,projection"/>-->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css"  media="screen,projection" />
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    
-      <script type="text/javascript" src="./materialize/js/materialize.min.js"></script>
-     <script type="text/javascript" src="./materialize/js/typehead.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
+      <!-- <script type="text/javascript" src="./materialize/js/materialize.min.js"></script>
+     <script type="text/javascript" src="./materialize/js/typehead.js"></script> -->
       <!--<link rel="stylesheet" href="materialize-tags.min.css">-->
+     <link rel="stylesheet" href="forms.css">
 
 </head>
-<link rel="stylesheet" href="forms.css">
  <!--  Begining of Navbar Header part  (# START) -->
 
 
