b3f2057608a550ca928d7abe5e5b2cfbbad7ba53
Pintu Lal <pintulalmee@gmail.com>
2015-07-16 23:15:55 +0530

    favicon

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <title>Jefto.com </title>
-  <link rel="icon" type="image/png" href="def_img/favicon.png">
+  <link rel="icon" type="image/png" href="def_img/favicon.png"/>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="author" content="Pintu LAl Meena">
