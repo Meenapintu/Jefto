9f63b41b999458161637f2dab1c3057036192757
Pintu Lal <pintulalmee@gmail.com>
2015-07-31 02:04:06 +0530

    debug

--- phpfunc.php
+++ phpfunc.php
@@ -13,8 +13,8 @@
         $rs = pg_execute($conn,$sqlName,$insert_array[$i]);
         if($rs == FALSE){
         //echo"</br> sorry we caught some kind of error! </br>";
-       // die("Can't prepare '$sql': " . pg_last_error());
-         die("We are facing some technical difficulties, please bear with us.");
+        die("Can't prepare '$sql': " . pg_last_error());
+        // die("We are facing some technical difficulties, please bear with us.");
         //echo false;
     }
     else{
@@ -26,8 +26,8 @@
     if(!pg_query($sql))
     {
 
-         die("We are facing some technical difficulties, please bear with us.");
-        //die("Can't query '$sql': " . pg_last_error());
+         //die("We are facing some technical difficulties, please bear with us.");
+        die("Can't query '$sql': " . pg_last_error());
     }
     //echo" add done multi";
     
@@ -46,10 +46,11 @@ function pg_prepare_single_insert($conn, $query,$insert_array)
     if($rs == FALSE){
         //echo"</br> sorry we caught some kind of error! </br>";
         ////echo"</br> THERE IS ERROR IN INSERT 1 </br>";
-        //die("Can't prepare '$sql': " . pg_last_error());
+        
         //echo false;
+        die("Can't prepare '$sql': " . pg_last_error());
         return false;
-         die("We are facing some technical difficulties, please bear with us.");
+        // die("We are facing some technical difficulties, please bear with us.");
     }
         //echo true;
     //}
@@ -58,7 +59,7 @@ function pg_prepare_single_insert($conn, $query,$insert_array)
     $sql = sprintf('DEALLOCATE "%s"',pg_escape_string($sqlName));
     if(!pg_query($sql))
     {
-        //die("Can't query '$sql': " . pg_last_error());
+        die("Can't query '$sql': " . pg_last_error());
        // return false;
            return false;
     }
@@ -72,16 +73,16 @@ function pg_prepare_single_insert_v1($conn, $query,$insert_array)
     $sqlName = 'event_insert_q';
     if (!pg_prepare ($conn,$sqlName, $query))
     {
-         die("We are facing some technical difficulties, please bear with us.");
-       // die("Can't prepare '$sql': " . pg_last_error());
+         //die("We are facing some technical difficulties, please bear with us.");
+        die("Can't prepare '$sql': " . pg_last_error());
     }
     $rs = pg_execute($conn,$sqlName,$insert_array);
     if($rs == FALSE){
         ////echo"</br> THERE IS ERROR IN INSERT 1 </br>";
-       // die("Can't prepare '$sql': " . pg_last_error());
+        die("Can't prepare '$sql': " . pg_last_error());
          //die("Sorry we caugh an error");
         //echo false;
-          die("We are facing some technical difficulties, please bear with us.");
+         // die("We are facing some technical difficulties, please bear with us.");
     }
     else{
 
@@ -92,7 +93,7 @@ function pg_prepare_single_insert_v1($conn, $query,$insert_array)
     $sql = sprintf('DEALLOCATE "%s"',pg_escape_string($sqlName));
     if(!pg_query($sql))
     {
-        //die("Can't query '$sql': " . pg_last_error());
+        die("Can't query '$sql': " . pg_last_error());
         //die("We are facing some technical difficulties, please bear with us.");
 
     }
