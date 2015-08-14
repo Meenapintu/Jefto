7873378ef4e17b353f8ce3245d29be3f19fb5349
Ram <ramchandratnapura@gmail.com>
2015-08-14 14:59:29 +0530

    changed desc color

--- profile.php
+++ profile.php
@@ -664,7 +664,7 @@ function offer_fire( $v ,$der,$rel){
 							<div class='row card-panel' style='background:rgba(34,53,53,0);overflow:none;width:100%;margin:0px;padding:0px;'>
 								<div class='col s12 m12 s12' style='background:rgba(34,53,3,0); color:green;margin-bottom:10px;'>
 								</div>
-							    <div  id='desc' class='col s12 m12 s12 card-panel'   style='word-wrap: break-word;'> 
+							    <div  id='desc' class='col s12 m12 s12 card-panel'   style='word-wrap: break-word;color:black;'> 
 							    	<?php echo $v[$i][description] ?> 
 							    </div>
 							    <div class='col s12 m12 s12 center ' id ='state'> 
