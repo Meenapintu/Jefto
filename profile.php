bff276d5047b9976f93432e2464ea7e432444afd
Ram <ramchandratnapura@gmail.com>
2015-08-25 16:42:39 +0530

    <i style

--- profile.php
+++ profile.php
@@ -643,11 +643,11 @@ function offer_fire( $v ,$der,$rel){
         		<div class="col s12 m12 l12 white-t pd0 margin-0">
         			<div onclick='setdrop(this);' >
         				<i class='material-icons '>description</i> 
-        				<div class="card-panel" id='descme' style='width:100%;height:auto;max-height:100%;margin:0px;padding:0px;position:absolute;display:none;background:white;left:0px;z-index:2'>
-							<div class='row card-panel' style='background:rgba(34,53,53,0);width:100%;margin:0px;padding:0px;'>
+        				<div class="card-panel" id='descme' style='width:100%;height:auto;overflow: auto;max-height:100%;margin:0px;padding:0px;position:absolute;display:none;background:white;left:0px;z-index:2'>
+							<div class='row card-panel' style='background:rgba(34,53,53,0);overflow: auto;width:100%;max-height:100%;margin:0px;padding:0px;'>
 								<div class='col s12 m12 s12' style='background:rgba(34,53,3,0); color:green;margin-bottom:10px;'>
 								</div>
-							    <div  id='desc' class='col s12 m12 s12'   style='word-wrap: break-word;color:black;'> 
+							    <div  id='desc' class='col s12 m12 s12'   style='word-wrap:break-word;color:black;overflow: auto;'> 
 							    	<p><?php echo $v[$i]['description'] ?> </p>
 							    </div>
 							    <div class='col s12 m12 s12 center ' id ='state'> 
