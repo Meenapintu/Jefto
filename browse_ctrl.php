2d09f6e603d2949bc2c0a0aaeed94862f20a9488
Ram <ramchandratnapura@gmail.com>
2015-08-30 03:26:42 +0530

    min of pack

--- browse_ctrl.php
+++ browse_ctrl.php
@@ -4,7 +4,7 @@ require_once('phpfunc.php');
 $event_id = array();
 $query = "SELECT event.event_id,event.name,event.city,event.address,event.country,event.website,event.start_date,event.end_date,event.link_for_req,event.budget,eventplus.logo,eventplus.total_audience FROM event left  JOIN eventplus ON event.event_id = eventplus.event_id";
 $event = pg_prepare_SELECT($conn,$query,$event_id);
-$query = "SELECT event_id,offer_id,offer_name,MIN(cost),currency from offers group by event_id";
+$query = "SELECT event_id,offer_id,offer_name,MIN(cost)as cost,currency from offers group by event_id";
 $offer = pg_prepare_SELECT($conn,$query,$event_id);
 
 function get_offer($event_id,$offer){
