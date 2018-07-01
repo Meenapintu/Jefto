d59290ba4ca5d0653e9b88bfaf2f2265de1ae032
Pintu Meena <pintulalmee@gmail.com>
2018-07-01 12:17:30 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php 
 	require_once("psp.php");
-	$connection_string = "host=localhost  dbname=jefto user=ram password=dfedderftrfr23";
+	$connection_string = "host=ec2-54-217-250-0.eu-west-1.compute.amazonaws.com  dbname=d73g42v8d095dg user=fcmhpgjorxzufu password=02658abc9e07a08442bb2560b5e3e5726226f804e4bf4a9bff5e32d4770594cd";
 	try {
 		$conn = pg_connect($connection_string);
 	} catch (Exception $e) {
