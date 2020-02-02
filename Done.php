bb7b8ed1355c701a878131d4fb29574295f42d06
Pintu Meena <pintulalmee@gmail.com>
2020-02-02 16:20:59 +0530

    adding blog

--- Done.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	require_once("header.php");
-	require_once('find.php');
-	require_once('footer.php');
-?>
-
-
-
--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-//echo $_GET['e'];
-$connection_string = "host=ec2-54-217-250-0.eu-west-1.compute.amazonaws.com  dbname=d73g42v8d095dg user=fcmhpgjorxzufu password=02658abc9e07a08442bb2560b5e3e5726226f804e4bf4a9bff5e32d4770594cd";
-$conn = pg_connect($connection_string);
-if(!$conn)//{ //echo "done ";}
-	echo " there is error ";
-
-?>
